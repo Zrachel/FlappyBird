c4bb8c05329eaae48b7a376c687fe27549b545f5
Zrachel <zrqsophia@sina.com>
2015-12-23 00:33:43 +0800

    initial file

---
+++ flappy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flappy
+//
+//  Created by Rachel on 15/12/19.
+//  Copyright © 2015年 Rachel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ flappy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flappy
+//
+//  Created by Rachel on 15/12/19.
+//  Copyright © 2015年 Rachel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
